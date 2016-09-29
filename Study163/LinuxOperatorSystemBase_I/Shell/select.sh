471da02d2fee9cd05ddf5bf8096a474cb4b8b51c
flyer <flyer_son@126.com>
2016-09-29 22:40:39 +0800

    分支代码

---
+++ Study163/LinuxOperatorSystemBase_I/Shell/select.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#选择分支练习
+#测试参数是否有可执行权限,如果有,输出ok,否则输出fail
+test -x $1 && echo "$1 ok" || echo "fail"
+
+#if 后面的中括号前后必须要有空格,否则会报错
+if [ -x $1 ]
+then
+    echo "$1 is executable"
+elif [ -x $2 ]
+then
+    echo "$1 is not executable but $2 is executable"
+else
+    echo "$1 and $2 is not executable"
+fi
+
+#赋值符号中间不能有空格
+val=$3
+case $val in
+    a)
+        echo "val is A"
+        ;;
+    b)
+        echo "val is B"
+        ;;
+    *)
+        echo "val is other"
+        ;;
+esac
+
+
---
+++ Study163/LinuxOperatorSystemBase_I/Shell/text.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#测试. text.sh 运行跟./text.sh 的差距
+ls -l ~/
+echo "$$,$0"
