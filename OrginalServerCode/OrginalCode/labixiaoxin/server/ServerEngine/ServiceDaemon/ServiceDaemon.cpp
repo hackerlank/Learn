0435bc5353d487c67632e8142c89d54dd3d528c6
flyer <flyer_son@126.com>
2016-11-30 21:11:45 +0800

    蜡笔小新

---
+++ OrginalServerCode/OrginalCode/labixiaoxin/server/ServerEngine/ServiceDaemon/ServiceDaemon.cpp
@@ -0,0 +1,15 @@
+// ServiceDaemon.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "..\Base\Service\BaseService.h"
+#include "ace\OS_main.h"
+#include "ace\ACE.h"
+
+
+int ACE_TMAIN(int argc, ACE_TCHAR **argv)
+{
+	return 0;
+}
+
